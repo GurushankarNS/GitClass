8438d57e4fcd9aa44a2272150bb5cae221166016
GurushankarNS <nsgurushankar24@gmail.com>
2025-07-30 17:46:07 +0530

    Testdone

---
+++ src/org/Gitproject/Day1Git.java
@@ -0,0 +1,18 @@
+package org.Gitproject;
+
+
+
+public class Day1Git {
+
+	private void GitClass() {
+		System.out.println("GitClassone");
+
+		
+	}
+	public static void main(String[] args) {
+		Day1Git objClass = new Day1Git();
+		objClass.GitClass();
+	}
+	
+	
+}
